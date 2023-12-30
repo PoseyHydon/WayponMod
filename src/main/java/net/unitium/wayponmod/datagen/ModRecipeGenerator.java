bb86eb51a8da0057325711652b28fc36b3afe2db
AmamiyaShiro <e.sonnois@gmail.com>
2023-12-30 19:48:54 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/generated/.cache/bf0fe89c9e97a23d664ce8a5e8b1e6bc0786faa0
    #	src/main/generated/.cache/c13cb800b4dc32c89cef15653da3cb10d8eb1a49
    #	src/main/java/net/unitium/wayponmod/item/ModItemsGroup.java

--- src/main/java/net/unitium/wayponmod/datagen/ModRecipeGenerator.java
+++ src/main/java/net/unitium/wayponmod/datagen/ModRecipeGenerator.java
@@ -3,6 +3,11 @@ package net.unitium.wayponmod.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import net.unitium.wayponmod.item.ModItems;
 
 import java.util.function.Consumer;
 
@@ -13,6 +18,14 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.STONE_HANDLE)
+                .pattern("XXX")
+                .pattern(" X ")
+                .input('X', Items.STONE)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.STONE_HANDLE) + "_"));
+
 
     }
 }
