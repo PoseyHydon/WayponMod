226531403f8b89eba6b90ac684bc129e1bd0192d
AmamiyaShiro <e.sonnois@gmail.com>
2023-12-30 20:34:08 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/generated/.cache/bf0fe89c9e97a23d664ce8a5e8b1e6bc0786faa0
    #	src/main/generated/.cache/c13cb800b4dc32c89cef15653da3cb10d8eb1a49
    #	src/main/java/net/unitium/wayponmod/item/ModItemsGroup.java

--- src/main/java/net/unitium/wayponmod/datagen/ModRecipeGenerator.java
+++ src/main/java/net/unitium/wayponmod/datagen/ModRecipeGenerator.java
@@ -21,6 +21,80 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
         handleMaker(Items.STONE, ModItems.STONE_HANDLE, exporter);
+        handleMaker(Items.OBSIDIAN, ModItems.OBSIDIAN_HANDLE, exporter);
+        
+        
+        rapiereBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.STONE, ModItems.STONE_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.IRON_INGOT, ModItems.IRON_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.DIAMOND, ModItems.DIAMOND_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.EMERALD, ModItems.EMERALD_RAPIERE_BLADE, exporter);
+        rapiereBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_RAPIERE_BLADE, exporter);
+
+        bigSwordBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.STONE, ModItems.STONE_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.IRON_INGOT, ModItems.IRON_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.DIAMOND, ModItems.DIAMOND_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.EMERALD, ModItems.EMERALD_BIG_SWORD_BLADE, exporter);
+        bigSwordBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_BIG_SWORD_BLADE, exporter);
+
+        katanaBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.STONE, ModItems.STONE_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.IRON_INGOT, ModItems.IRON_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.DIAMOND, ModItems.DIAMOND_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.EMERALD, ModItems.EMERALD_KATANA_BLADE, exporter);
+        katanaBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_KATANA_BLADE, exporter);
+
+        glaiveBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.STONE, ModItems.STONE_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.IRON_INGOT, ModItems.IRON_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.DIAMOND, ModItems.DIAMOND_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.EMERALD, ModItems.EMERALD_GLAIVE_BLADE, exporter);
+        glaiveBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_GLAIVE_BLADE, exporter);
+
+        fauxBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.STONE, ModItems.STONE_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.IRON_INGOT, ModItems.IRON_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.DIAMOND, ModItems.DIAMOND_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.EMERALD, ModItems.EMERALD_FAUX_BLADE, exporter);
+        fauxBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_FAUX_BLADE, exporter);
+
+        katarBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.STONE, ModItems.STONE_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.IRON_INGOT, ModItems.IRON_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.DIAMOND, ModItems.DIAMOND_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.EMERALD, ModItems.EMERALD_KATAR_BLADE, exporter);
+        katarBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_KATAR_BLADE, exporter);
+
+        spearBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.STONE, ModItems.STONE_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.IRON_INGOT, ModItems.IRON_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.DIAMOND, ModItems.DIAMOND_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.EMERALD, ModItems.EMERALD_SPEAR_BLADE, exporter);
+        spearBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_SPEAR_BLADE, exporter);
+
+        axeBladeMaker(Items.OAK_PLANKS, ModItems.WOOD_AXE_BLADE, exporter);
+        axeBladeMaker(Items.STONE, ModItems.STONE_AXE_BLADE, exporter);
+        axeBladeMaker(Items.GOLD_INGOT, ModItems.GOLD_AXE_BLADE, exporter);
+        axeBladeMaker(Items.IRON_INGOT, ModItems.IRON_AXE_BLADE, exporter);
+        axeBladeMaker(Items.COPPER_INGOT, ModItems.COPPER_AXE_BLADE, exporter);
+        axeBladeMaker(Items.DIAMOND, ModItems.DIAMOND_AXE_BLADE, exporter);
+        axeBladeMaker(Items.EMERALD, ModItems.EMERALD_AXE_BLADE, exporter);
+        axeBladeMaker(Items.NETHERITE_INGOT, ModItems.NETHERITE_AXE_BLADE, exporter);
 
     }
 
@@ -45,9 +119,9 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     public void bigSwordBladeMaker(Item item, Item output, Consumer<RecipeJsonProvider> exporter){
         ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output)
-                .pattern("XX")
-                .pattern("XX")
-                .pattern("XX")
+                .pattern(" XX")
+                .pattern("XXX")
+                .pattern("XX ")
                 .input('X', item)
                 .criterion(hasItem(item), conditionsFromItem(item))
                 .offerTo(exporter, new Identifier(getRecipeName(output) + "_"));
@@ -63,8 +137,8 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     }
     public void glaiveBladeMaker(Item item, Item output, Consumer<RecipeJsonProvider> exporter){
         ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output)
-                .pattern("X")
-                .pattern("X")
+                .pattern("  X")
+                .pattern(" X ")
                 .input('X', item)
                 .criterion(hasItem(item), conditionsFromItem(item))
                 .offerTo(exporter, new Identifier(getRecipeName(output) + "_"));
@@ -80,8 +154,9 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     }
     public void katarBladeMaker(Item item, Item output, Consumer<RecipeJsonProvider> exporter){
         ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output)
-                .pattern(" X ")
-                .pattern("X  ")
+                .pattern("  X")
+                .pattern("XX ")
+                .pattern("XX ")
                 .input('X', item)
                 .criterion(hasItem(item), conditionsFromItem(item))
                 .offerTo(exporter, new Identifier(getRecipeName(output) + "_"));
